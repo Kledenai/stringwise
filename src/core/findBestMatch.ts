21eaa91fe0ca9e62173e757c90e6b2070a37685b
Bruno Rocha <bruno.rocha@beetech.com.br>
2025-04-20 17:00:05 -0300

    Add round option in findBestMatch function

--- src/core/findBestMatch.ts
+++ src/core/findBestMatch.ts
@@ -4,22 +4,31 @@ import type { MatchRating } from '../types';
 
 export function findBestMatch(
 	mainString: string,
-	targets: string[]
+	targets: string[],
+	options?: { round?: number }
 ): {
 	bestMatch: MatchRating;
 	bestMatchIndex: number;
 	ratings: MatchRating[];
 } {
+
 	if (!isValidInput(mainString, targets)) {
 		throw new Error(
 			'Invalid arguments: expected a string and an array of strings'
 		);
 	}
 
-	const ratings = targets.map((target) => ({
-		target,
-		rating: compareTwoStrings(mainString, target)
-	}));
+	const precision = options?.round;
+
+  const ratings = targets.map((target) => {
+    const rating = compareTwoStrings(mainString, target);
+    const rounded = typeof precision === 'number' ? Number(rating.toFixed(precision)) : rating;
+
+    return {
+      target,
+      rating: rounded
+    };
+  });
 
 	const bestMatchIndex = ratings.reduce(
 		(bestIndex, current, i, arr) =>
