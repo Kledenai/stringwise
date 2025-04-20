f8c25f5afcf30c85861a892906d9d936f4563ec2
Bruno Rocha <bruno.rocha@beetech.com.br>
2025-04-20 17:25:44 -0300

    Update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,21 @@
+export type SimilarityAlgorithm =
+  | 'default'
+  | 'levenshtein'
+  | 'jaro-winkler'
+  | 'ratcliff-obershelp';
+
 export interface MatchRating {
-	target: string;
-	rating: number;
+  target: string;
+  rating: number;
+}
+
+export interface FindBestMatchOptions {
+  round?: number;
+  algorithm?: SimilarityAlgorithm;
+}
+
+export interface FindBestMatchResult {
+  bestMatch: MatchRating;
+  bestMatchIndex: number;
+  ratings: MatchRating[];
 }
