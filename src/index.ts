212c625bd4ffa4c95fa7b9683020ab8e222555b0
Bruno Rocha <bruno.rocha@beetech.com.br>
2025-04-20 17:25:04 -0300

    expose get similarity fn function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { findBestMatch } from './core/findBestMatch';
+export { getSimilarityFn } from './core/similarity';
 export { compareTwoStrings } from './core/compare';
 export type * from './types';
