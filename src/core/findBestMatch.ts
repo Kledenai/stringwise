8216a0c5cd354929d6bae342b9659b2c0ce3a062
Bruno Rocha <bruno.rocha@beetech.com.br>
2025-04-20 17:25:15 -0300

    Add improvements in find best match

--- src/core/findBestMatch.ts
+++ src/core/findBestMatch.ts
@@ -1,28 +1,29 @@
 import isValidInput from '../utils/isValidInput';
-import { compareTwoStrings } from './compare';
-import type { MatchRating } from '../types';
+import { getSimilarityFn } from './similarity';
+import type {
+  FindBestMatchOptions,
+  FindBestMatchResult,
+  MatchRating
+} from '../types';
 
 export function findBestMatch(
-	mainString: string,
-	targets: string[],
-	options?: { round?: number }
-): {
-	bestMatch: MatchRating;
-	bestMatchIndex: number;
-	ratings: MatchRating[];
-} {
+  mainString: string,
+  targets: string[],
+  options?: FindBestMatchOptions
+): FindBestMatchResult {
+  if (!isValidInput(mainString, targets)) {
+    throw new Error(
+      'Invalid arguments: expected a string and an array of strings'
+    );
+  }
 
-	if (!isValidInput(mainString, targets)) {
-		throw new Error(
-			'Invalid arguments: expected a string and an array of strings'
-		);
-	}
+  const precision = options?.round;
+  const similarityFn = getSimilarityFn(options?.algorithm || 'default');
 
-	const precision = options?.round;
-
-  const ratings = targets.map((target) => {
-    const rating = compareTwoStrings(mainString, target);
-    const rounded = typeof precision === 'number' ? Number(rating.toFixed(precision)) : rating;
+  const ratings: MatchRating[] = targets.map((target) => {
+    const rating = similarityFn(mainString, target);
+    const rounded =
+      typeof precision === 'number' ? Number(rating.toFixed(precision)) : rating;
 
     return {
       target,
@@ -30,16 +31,15 @@ export function findBestMatch(
     };
   });
 
-	const bestMatchIndex = ratings.reduce(
-		(bestIndex, current, i, arr) =>
-			current.rating > arr[bestIndex].rating ? i : bestIndex,
-		0
-	);
+  const bestMatchIndex = ratings.reduce(
+    (bestIndex, current, i, arr) =>
+      current.rating > arr[bestIndex].rating ? i : bestIndex,
+    0
+  );
 
-	return {
-		ratings,
-		bestMatch: ratings[bestMatchIndex],
-		bestMatchIndex
-	};
+  return {
+    ratings,
+    bestMatch: ratings[bestMatchIndex],
+    bestMatchIndex
+  };
 }
-
